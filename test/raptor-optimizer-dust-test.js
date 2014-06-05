9855f6779d6aa67cd5651b078c28e505f44a8bad
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-04 22:00:46 -0600

    Initial commit

---
+++ test/raptor-optimizer-dust-test.js
@@ -0,0 +1,34 @@
+'use strict';
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+
+
+describe('raptor-optimizer-dust' , function() {
+
+    beforeEach(function(done) {
+        // for (var k in require.cache) {
+        //     if (require.cache.hasOwnProperty(k)) {
+        //         delete require.cache[k];
+        //     }
+        // }
+
+        done();
+    });
+
+    it('should correctly compile a Dust template to a CommonJS module', function(done) {
+        var compiler = require('../lib/dust-compiler');
+        var templatePath = require.resolve('./template.dust');
+        compiler.compileFile(templatePath, function(err, compiledSrc) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            console.log('compiledSrc:\n' + compiledSrc);
+            done();
+        });
+    });
+
+});
